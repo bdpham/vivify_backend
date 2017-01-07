50e2288c711e27013a5e469c194d7bc411e212f4
bdpham <bdpham93@gmail.com>
2017-01-07 00:52:45 -0500

    added collaborative playlist scope

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ app.get('/login', function(req, res) {
   // res.cookie(stateKey, state);
 
   // your application requests authorization
-  var scope = 'user-read-private user-read-email playlist-read-private';
+  var scope = 'user-read-private user-read-email playlist-read-private playlist-read-collaborative';
   res.redirect('https://accounts.spotify.com/authorize?' +
     querystring.stringify({
       response_type: 'code',
